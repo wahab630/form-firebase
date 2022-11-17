b5295c154956cabd6dadcb2a544c19a17cdd9e8d
wahab630 <wahabmalik630@gmail.com>
2022-11-17 17:07:39 +0500

    added

---
+++ src/components/elements/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+    <div className="contaier">
+        <div className="row">
+            
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Hero
